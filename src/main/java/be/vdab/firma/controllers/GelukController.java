ad3ba6ef72466aabf9d8ded58b1de09f9752d9a9
dmtG <dimitri.gevers@gmail.com>
2020-09-15 21:09:25 +0200

    spring advanced exc 2.4 firma

---
+++ src/main/java/be/vdab/firma/controllers/GelukController.java
@@ -0,0 +1,16 @@
+package be.vdab.firma.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@PreAuthorize("hasAuthority('gebruiker')")
+@RequestMapping("/geluk")
+class GelukController {
+    @GetMapping
+    public String geluk(){
+        return "geluk";
+    }
+}
